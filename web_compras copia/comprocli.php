1a6d1ddc6f5f8a6b6698ff894e1230f0f9439e9b
SoyLin <145429706+SoyLin@users.noreply.github.com>
2024-01-16 08:14:12 +0100

    Add files via upload

---
+++ web_compras copia/comprocli.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>COMPRAS</title>
+</head>
+<body>
+<a href="./portalcli.php">Volver al menú</a>
+
+</body>
+</html>
